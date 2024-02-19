d157dd08256f2b2267fa5da2c6a64197c1f58849
eruchandu <chandueru2002@example.com>
2024-02-20 01:13:46 +0530

    updated login

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -4,12 +4,13 @@ import axios from 'axios';
 import AuthContext from '../contexts/AuthContext.js';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Forget from './Forget.js'
 import SERVER from '../applink.js'
 function Login(){
   let [User,SetUser]=useState({empid:'',password:''});
-
 const { user, isLoggedIn, login, logout} = useContext(AuthContext);
-
+const [showModal, setShowModal] = useState(false);
+const [passMOdal,setPassModal]=useState(false);
 const navigate=useNavigate('');
 function submitHandler(event)
 {
@@ -36,16 +37,24 @@ function submitHandler(event)
     
 
 }
+const handleCloseModal = () => {
+  setShowModal(false);
+};
+function handleForgetClick(event)
+{
+  setShowModal(true);
+}
 
 return(
-     <div className='border border-5 w-50 mx-auto mt-5 pb-4 shadow'>
+     <div className='border border-5 w-50 mx-auto mt-5 pb-4 shadow rounded'>
+      <div className='login-form mx-auto w-75 mt-5 d-flex'>
+        <img src='../profile.svg' className='w-75 mx-auto'></img>
+      </div>
     <div className='login-form mx-auto w-75 mt-5 '>
        <form onSubmit={submitHandler}>
   <div class="form-group mt-5" >
     <label for="empid">Employee id</label>
     <input type="text" class="form-control mt-3 mb-4" id="empid" name="empid" onChange={(e)=>SetUser({...User,empid:e.target.value})} ></input>
-    
-    
   </div>
   <div class="form-group">
     <label for="Password">Password</label>
@@ -54,8 +63,13 @@ return(
   
   <button type="submit" class="btn btn-primary d-block mx-auto">Submit</button>
 </form>
-
+<div>
+  <button className="d-block mx-auto mt-4 mx-2" onClick={() => handleForgetClick()} style={{background:"none",border:"none",textDecoration:"underline 1px blue",textUnderlineOffset:"5px"}}> Forgot Password ? </button>
+  </div>
     </div>
+    {showModal && (
+        <Forget onClose={handleCloseModal} />
+      )}
     </div>
     
 )
